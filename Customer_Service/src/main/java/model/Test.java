f3e5bbbbb1b1a83d753747f313686032d5ccc44f
IT20207854 <maleeshadesilva21@gmail.com>
2022-04-24 12:04:58 +0530

    Test commit

--- Customer_Service/src/main/java/model/Test.java
+++ Customer_Service/src/main/java/model/Test.java
@@ -1,5 +1,23 @@
 package model;
 
-public class Test {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Test {
+	private Connection connect() 
+	{ 
+		Connection con = null; 
+		try
+		{ 
+			Class.forName("com.mysql.jdbc.Driver"); 
+				 
+			//Provide the correct details: DBServer/DBName, username, password 
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/electro_grid", "root", ""); 
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		} 
+		return con; 
+	} 
 }
